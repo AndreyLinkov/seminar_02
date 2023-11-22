9b3b117168de775d2f6e31cb662f2adcb7bdb60e
Andrey Linkov <linkov.developer@inbox.ru>
2023-11-22 20:31:10 +0300

    task_01

---
+++ task_01/Program.cs
@@ -0,0 +1,24 @@
+﻿int CoordinateNumber (int X, int Y) {
+    if (X > 0 && Y > 0) {
+        return 1;
+    }
+    else if (X < 0 && Y > 0) {
+        return 2;
+    }
+    else if (X < 0 && Y < 0) {
+        return 3;
+    }
+    else {
+        return 4;
+    }
+}
+
+Console.Write("Введите координату X: ");
+int coorX = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите координату Y: ");
+int coorY = Convert.ToInt32(Console.ReadLine());
+
+int coordinate = CoordinateNumber(coorX, coorY);
+
+Console.WriteLine($"Координата находится на {coordinate} плоскости");
