c8df7ec6c623ea9daadf4f041fd12e4ec881a508
Andrey Linkov <linkov.developer@inbox.ru>
2023-11-22 20:41:34 +0300

    task_02

---
+++ task_02/Program.cs
@@ -0,0 +1,20 @@
+﻿bool MultipleNumber (int num) {
+    if (num%7==0 && num%23==0) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+bool multiple = MultipleNumber(number);
+
+if (multiple == true) {
+    Console.WriteLine($"Введенное число {number} кратно 7 и 23");
+}
+else {
+    Console.WriteLine($"Введенное число {number} не кратно 7 и 23");
+}
