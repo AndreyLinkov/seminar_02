ecbf3c0e04b7fc27c1f8c07cd2b64ce7dbdf1800
Andrey Linkov <linkov.developer@inbox.ru>
2023-11-24 22:29:08 +0300

    add task_04

---
+++ task_04/Program.cs
@@ -0,0 +1,20 @@
+﻿int NumeralNumber(int num) {
+    string numStr = num.ToString();
+    char[] digits = numStr.ToCharArray();
+    int i=0;
+
+    while (i<digits.Length) {
+        Console.Write(numStr[i]);
+
+        if (i<digits.Length-1) {
+            Console.Write(", ");
+        }
+        i++;
+    }
+    return 0;
+}
+
+Console.Write("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+NumeralNumber(number);
