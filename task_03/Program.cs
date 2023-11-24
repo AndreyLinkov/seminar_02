ec1537bbf467c1266e38f8b960d281db498c6d81
Andrey Linkov <linkov.developer@inbox.ru>
2023-11-24 20:54:39 +0300

    add task_03

---
+++ task_03/Program.cs
@@ -0,0 +1,26 @@
+﻿int NumeralNumber(int num) {
+
+    string numStr = num.ToString();
+    if (numStr[0] > numStr[1]) {
+        Console.WriteLine($"Цифра {numStr[0]} больше, чем {numStr[1]}");
+        return 1;
+    }
+    else if (numStr[0] == numStr[1]) {
+        Console.WriteLine($"Цифры {numStr[0]} и {numStr[1]} равны");
+        return 0;
+    }
+    else {
+        Console.WriteLine($"Цифра {numStr[1]} меньше, чем {numStr[0]}");
+        return 2;
+    }
+}
+
+Console.Write("Введите двузначное положительное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+while (number < 10 || number > 99) {
+    Console.Write("Ошибка. Введите двузначное положительное число: ");
+    number = Convert.ToInt32(Console.ReadLine());
+}
+
+int numeralNumber = NumeralNumber(number);
