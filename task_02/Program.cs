5cafae32de5928b4943145c978a5a6ec477e1939
Andrey Linkov <linkov.developer@inbox.ru>
2024-02-20 10:58:15 +0300

    fix task_02

--- task_02/Program.cs
+++ task_02/Program.cs
@@ -7,9 +7,45 @@ namespace task_02
 {
     class Program
     {
+        public static void Quarter(int coorX, int coorY)
+        {
+            if (coorX > 0 && coorY > 0)
+                Console.WriteLine("Координаты находятся на 1 четверти");
+            if (coorX > 0 && coorY < 0)
+                Console.WriteLine("Координаты находятся на 2 четверти");
+            if (coorX < 0 && coorY < 0)
+                Console.WriteLine("Координаты находятся на 3 четверти");
+            if (coorX < 0 && coorY > 0)
+                Console.WriteLine("Координаты находятся на 4 четверти");
+        }
+
         static void Main()
         {
             Console.Clear();
+
+            int coorX;
+            int coorY;
+            bool inputError = true;
+
+            do
+            {
+                Console.Write("Введите координаты точки X не равные 0: ");
+                string input = Console.ReadLine()!;
+                if (int.TryParse(input, out coorX) && coorX != 0)
+                    inputError = false;
+            } while (inputError);
+
+            inputError = true;
+
+            do
+            {
+                Console.Write("Введите координаты точки Y не равные 0: ");
+                string input = Console.ReadLine()!;
+                if (int.TryParse(input, out coorY) && coorY != 0)
+                    inputError = false;
+            } while (inputError);
+
+            Quarter(coorX, coorY);
         }
     }
 }
